e3b1d3c40b83ada368992aa10105cbffabac596c
meridaKim <sujin7187@naver.com>
2023-12-19 15:48:47 +0900

    [UPDATE] 백준 5567 결혼식 문제풀이

--- src/BOJ/BOJ_5567.java
+++ src/BOJ/BOJ_5567.java
@@ -3,13 +3,11 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
-import sun.awt.image.ImageWatched.Link;
+
 
 /**
  * 결혼식 */
@@ -32,6 +30,7 @@ public class BOJ_5567 {
             int v2 = Integer.parseInt(st.nextToken());
             graph[v1][v2]=graph[v2][v1]=1;
         }
+        //방문 여부를 dept로 구분하므로 -1로 초기화
         Arrays.fill(dept,-1);
         bfs(1);
         System.out.println(count);
@@ -42,14 +41,19 @@ public class BOJ_5567 {
         dept[v] =0;
         while(!que.isEmpty()){
             int cur = que.poll();
-                if(dept[cur]<=2&&1<=dept[cur]) count++;
-                if(dept[cur]==2) continue;
-                    for(int i =0; i<graph[cur].length; i++){
-                        if(dept[i]!=-1) continue;
-                        que.offer(i);
-                        dept[i] = dept[cur]+1;
-                    }
-        }
+            //dept =0,1,2일때 현재 인물은 초대 가능하므로 count++;
+            if(-1<dept[cur]&&dept[cur]<3) count++;
+            //dept ==2이라면 현재 인물과 연결된 사람은 dept==3이 되므로 초대 불가
+            if(dept[cur]==2) continue;
+            for(int i =0; i<graph[cur].length; i++){
+                if(dept[i]==-1&&graph[cur][i]==1){
+                    dept[i] = dept[cur]+1;
+                    que.add(i);
+                }
+            }
+
+        }count--;
+        //상근이 자신을 제외해야 하므로 (dept==0이 포함됨) -1;
     }
 
 }
