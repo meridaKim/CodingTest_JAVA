f70ee7a0a85daf63bc1e1019468722b5e149aab5
meridaKim <71478325+meridaKim@users.noreply.github.com>
2024-01-03 23:36:37 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 264 ms, Memory: 26984 KB -BaekjoonHub

---
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+import java.util.HashSet;
+import java.util.Set;
+public class Main {
+    public static void main(String [] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        String game = st.nextToken();
+        int person = 0;
+        switch (game){
+            case "Y":
+                person = 2;
+                break;
+            case "F":
+                person = 3;
+                break;
+            case "O":
+                person = 4;
+                break;
+        }
+        HashSet<String> set = new HashSet<>();
+        for(int i =0; i<n; i++){
+            set.add(br.readLine());
+        }
+        System.out.print(set.size()/(person-1));
+    }
+}
