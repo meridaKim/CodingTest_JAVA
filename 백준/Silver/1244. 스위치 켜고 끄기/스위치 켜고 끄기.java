ae89dd4540af275444a298446d59cf372b6f86bf
meridaKim <71478325+meridaKim@users.noreply.github.com>
2024-01-09 20:49:35 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 148 ms, Memory: 14564 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,48 +1,56 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int total = Integer.parseInt(br.readLine()); 
-		int[] switches = new int[total];
-		StringTokenizer st = new StringTokenizer(br.readLine()); 
-		for(int i=0; i<total; i++)
-			switches[i] = Integer.parseInt(st.nextToken());
-		
-		int stuCnt = Integer.parseInt(br.readLine()); 
-		for(int i=0; i<stuCnt; i++) {
-			st = new StringTokenizer(br.readLine());
-			int gender = Integer.parseInt(st.nextToken());
-			int number = Integer.parseInt(st.nextToken());
-			
-			//남학생이면
-			if(gender == 1) {
-				for(int j=0; j<total; j++) 
-					if((j+1) % number == 0)
-						switches[j] = switches[j] == 0? 1: 0;
-			}
-			//여학생이면
-			else {
-				switches[number - 1] = switches[number - 1] == 0 ? 1 : 0;
-				for(int j=1; j<total/2; j++) {
-					if(number - 1 + j >= total || number - 1 - j < 0)
-						break;
-					if(switches[number - 1 - j] == switches[number - 1 + j]) {
-						switches[number - 1 - j] = switches[number - 1 - j] == 0 ? 1 : 0;
-						switches[number - 1 + j] = switches[number - 1 + j] == 0 ? 1 : 0;
-					}
-					else break; //대칭 아닌것이 나오면 바로 끝낸다.
-				}
-			}
-		}
-		
-		for(int i=0; i<total; i++) {
-			System.out.print(switches[i] + " ");
-			if((i+1) % 20 == 0)
-				System.out.println();
-		}
-	}
+    static StringTokenizer st;
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int [] arr = new int[n+1];
+        st = new StringTokenizer(br.readLine());
+        arr[0] = Integer.MAX_VALUE;
+        for(int i =1; i<n+1; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int s = Integer.parseInt(br.readLine());
+        for(int i =0; i<s; i++){
+            st = new StringTokenizer(br.readLine());
+                int gender = Integer.parseInt(st.nextToken());
+                int num = Integer.parseInt(st.nextToken());
+                if(gender==1){
+                    for(int k=num; k<arr.length; k++){
+                        if(k%num==0){
+                            if(arr[k]==0) arr[k]=1;
+                            else arr[k] =0;
+                        }
+                    }
+                }
+                if(gender==2){
+                        int start = num-1;
+                        int end = num+1;
+                        while(start>0&&end<n+1){
+                            if(arr[start]!=arr[end]){
+                                break;
+                            }
+                            start--;
+                            end++;
+                        }
+                        if(arr[start+1]==arr[end-1]) {
+                            for (int l = start+1; l <= end-1; l++) {
+                                if (arr[l] == 0) arr[l] = 1;
+                                else arr[l] = 0;
+                            }
+                        }
+                }
+            }
+
+        for(int i =1; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+            if(i%20==0) {
+                System.out.println();
+            }
+        }
+    }
 }
