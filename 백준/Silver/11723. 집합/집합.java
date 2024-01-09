3dd9b00d42f7a8aa884d15277076a2d52ba44cb5
meridaKim <71478325+meridaKim@users.noreply.github.com>
2024-01-09 22:34:10 +0900

    [Silver V] Title: 집합, Time: 1076 ms, Memory: 314460 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,51 +1,52 @@
 import java.io.*;
-import java.util.*;
-import java.lang.*;
-public class Main {	
-	public static void main(String[] args) throws NumberFormatException, IOException{
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		
-		int M = Integer.parseInt(in.readLine()); // 연산의 개수
-		HashSet<Integer> set = new HashSet<>();
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i=0;i<M;i++) {
-			StringTokenizer st = new StringTokenizer(in.readLine()," ");
-			String inst = st.nextToken(); // 명령어
-			int num=0; // 임시 초기화
-			
-			switch(inst) {
-				case "add":
-					num = Integer.parseInt(st.nextToken());
-					set.add(num);
-					break;
-				case "remove":
-					num = Integer.parseInt(st.nextToken());
-					set.remove(num);
-					break;
-				case "check":
-					num = Integer.parseInt(st.nextToken());
-					sb.append(set.contains(num)? "1":"0").append('\n');
-					break;
-				case "toggle":
-					num = Integer.parseInt(st.nextToken());
-					if(set.contains(num)) {
-						set.remove(num);
-					}else {
-						set.add(num);
-					}
-					break;
-				case "all":
-					set.clear();
-					for(int j=1;j<=20;j++)
-						set.add(j);
-					break;
-				case "empty":
-					set.clear();
-					break;
-			}
-		}
-		System.out.print(sb);
-	}
-	
+import java.util.StringTokenizer;
+
+// 비트 마스크 문제
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    // 출력을 위한 객체
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+       int n = Integer.parseInt(br.readLine());
+       int bitset = 0;
+
+       while(n-- > 0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String op = st.nextToken();
+            int num;
+
+            // 수행연산
+            switch (op){
+                case "add" :
+                    num = Integer.parseInt(st.nextToken());
+                    // num - 1인 이유는 20이 들어왔을 때
+                    // 2^19 자리가 20번째이기 때문이다.
+                    bitset |= (1 << (num - 1));
+                    break;
+                case "remove" :
+                    num = Integer.parseInt(st.nextToken());
+                    bitset = bitset & ~(1 << (num - 1));
+                    break;
+                case "check" :
+                    num = Integer.parseInt(st.nextToken());
+                    sb.append((bitset & (1 << (num - 1))) != 0 ? "1\n" : "0\n");
+                    break;
+                case "toggle" :
+                    num = Integer.parseInt(st.nextToken());
+                    bitset ^= (1 << (num - 1));
+                    break;
+                case "all" :
+                    bitset |= (~0);
+                    break;
+                case "empty" :
+                    bitset &= 0;
+                    break;
+            }
+        }
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
 }
