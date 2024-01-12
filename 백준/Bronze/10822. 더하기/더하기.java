8fd5d0a10c61454aad8facf1b3e6cae95bc7b83c
meridaKim <71478325+meridaKim@users.noreply.github.com>
2024-01-12 19:08:55 +0900

    [Bronze II] Title: 더하기, Time: 128 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/10822. 더하기/더하기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum =0;
+        String str = st.nextToken();
+        String [] arr = str.split(",");
+        for(int i =0; i<arr.length; i++){
+            sum+=Integer.parseInt(arr[i]);
+        }
+        System.out.println(sum);
+
+    }
+}
