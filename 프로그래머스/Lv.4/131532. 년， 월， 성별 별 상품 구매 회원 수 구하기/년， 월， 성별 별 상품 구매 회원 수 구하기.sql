b55f801d63386d1f978ae1c209c1016feac502aa
meridaKim <71478325+meridaKim@users.noreply.github.com>
2024-01-12 15:01:10 +0900

    [level Lv.4] Title: 년, 월, 성별 별 상품 구매 회원 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/Lv.4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
+++ 프로그래머스/Lv.4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
@@ -1,6 +1,16 @@
 -- 코드를 입력하세요
 SELECT DATE_FORMAT(o.sales_date,'%Y') as YEAR, DATE_FORMAT(o.sales_date,'%m') as MONTH, u.gender, count(distinct o.user_id) as USER
-FROM online_sale as o ,user_info as u
-where u.user_id = o.user_id and u.gender is not null
+FROM online_sale as o 
+JOIN user_info as u
+ON u.user_id = o.user_id and u.gender is not null
 group by DATE_FORMAT(o.sales_date,'%Y'),DATE_FORMAT(o.sales_date,'%m'),u.gender
 order by DATE_FORMAT(o.sales_date,'%Y'), DATE_FORMAT(o.sales_date,'%m'),u.gender;
+
+
+# SELECT YEAR(SALES_DATE) AS YEAR, MONTH(SALES_DATE) AS MONTH, USER_INFO.GENDER, COUNT( DISTINCT ONLINE_SALE.USER_ID) AS USERS
+# FROM ONLINE_SALE
+# JOIN USER_INFO
+# ON ONLINE_SALE.USER_ID = USER_INFO.USER_ID AND
+# GENDER IS NOT NULL
+# GROUP BY YEAR(SALES_DATE), MONTH(SALES_DATE), USER_INFO.GENDER 
+# ORDER BY YEAR(SALES_DATE), MONTH(SALES_DATE), USER_INFO.GENDER
